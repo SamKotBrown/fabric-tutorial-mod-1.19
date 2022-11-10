f5d5ba3e54df4d8ff96b4e41e6807f646a15927e
VaderVirusExe <smlbrown967@gmail.com>
2022-11-10 12:10:36 +0100

    Initial commit

---
+++ src/main/java/net/smlbr/tutorialmod/TutorialMod.java
@@ -0,0 +1,14 @@
+package net.smlbr.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String TUTORIALMOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(TUTORIALMOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
